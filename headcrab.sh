718b5c71290d98b09ca4ccc38f20fc4032fa5441
Deadboys Corner <48094316+Deadboy666@users.noreply.github.com>
2026-02-03 01:55:16 -0600

    Update headcrab.sh

--- headcrab.sh
+++ headcrab.sh
@@ -398,7 +398,7 @@ set -eu
         whereSLSsteamconfig
             if grep -q -F "PlayNotOwnedGames: no" "config.yaml"; then
                 sed -i "s/^PlayNotOwnedGames:.*/PlayNotOwnedGames: yes/" config.yaml
-                sed -i "s/^SafeMode:.*/SafeMode: no/" config.yaml
+                sed -i "s/^SafeMode:.*/SafeMode: yes/" config.yaml
                 echo "PlayNotOwnedGames: Enabled"
                 echo "SafeMode: Enabled"
             else
